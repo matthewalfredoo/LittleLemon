1f86c311f70fc291613b59a15024a7ccaff30139
Matthew Alfredo <matthewwalfredoo@gmail.com>
2023-05-08 15:41:53 +0700

    Week 2 - Chapter 1 - Event handling and callbacks

--- app/src/main/java/com/example/littlelemon/UpperPanel.kt
+++ app/src/main/java/com/example/littlelemon/UpperPanel.kt
@@ -1,15 +1,11 @@
 package com.example.littlelemon
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,34 +13,21 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.littlelemon.ui.theme.LittleLemonTheme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            LittleLemonTheme {
-                UpperPanel();
-            }
-        }
-    }
-}
 
 @Composable
-fun MainComponent() {
+fun UpperPanel(){
+    val context = LocalContext.current
     Column(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start,
@@ -85,27 +68,14 @@ fun MainComponent() {
             )
         }
         Button(
-            onClick = { },
+            onClick = {
+                      Toast.makeText(context, "Order successful!", Toast.LENGTH_SHORT).show()
+            },
             shape = RoundedCornerShape(10.dp),
             modifier = Modifier.padding(horizontal = 20.dp),
             colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFF4CE14))
         ) {
             Text(text = stringResource(id = R.string.order))
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LittleLemonTheme {
-        Greeting("Android")
     }
 }
